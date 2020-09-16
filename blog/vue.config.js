dc0b2d5755f63e7b61ecd37995f0f362f3d9c367
pearl <927082374@qq.com>
2020-09-16 19:15:48 +0800

    add vue.config.js

---
+++ blog/vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	/* 其他配置信息... */
+	publicPath: '/blog/'   /* 
+						  *  `publicPath` 里面的内容就是配置路径，如果你在服务器根目录下创建一个
+						  *  `newproject`文件夹，那么就需要配置成`/newproject/`。
+						  *  那么访问路径就是`www.test.com/newproject`
+						  */
+}
