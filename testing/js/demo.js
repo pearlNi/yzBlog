eeee050c41d145cbd0d672a95f9cbc17ff967142
pearl <927082374@qq.com>
2021-07-06 17:09:47 +0800

    fix

--- testing/js/demo.js
+++ testing/js/demo.js
@@ -347,11 +347,11 @@ wx.ready(function () {
   // 5.2 图片预览
   document.querySelector('#previewImage').onclick = function () {
     wx.previewImage({
-      current: 'http://img5.douban.com/view/photo/photo/public/p1353993776.jpg',
+      current: 'https://mmbiz.qpic.cn/mmbiz_jpg/M6nr7uuNic7tU2yAMibJrW12C1bFPOo6LIZQYoZECxd70geBu77vmlicqXnEX5AtWPia5F4x04CFyB2bVCzsyHhWEw/0?wx_fmt=jpeg',
       urls: [
-        'http://img3.douban.com/view/photo/photo/public/p2152117150.jpg',
-        'http://img5.douban.com/view/photo/photo/public/p1353993776.jpg',
-        'http://img3.douban.com/view/photo/photo/public/p2152134700.jpg'
+        'https://mmbiz.qpic.cn/mmbiz_jpg/M6nr7uuNic7tU2yAMibJrW12C1bFPOo6LIdTibqKIiba4iagQb12ezjGYr8elKcFxGrp3yiaqC1tfTMJ9X45ooYxvjFQ/0?wx_fmt=jpeg',
+        'https://mmbiz.qpic.cn/mmbiz_jpg/M6nr7uuNic7tU2yAMibJrW12C1bFPOo6LIZQYoZECxd70geBu77vmlicqXnEX5AtWPia5F4x04CFyB2bVCzsyHhWEw/0?wx_fmt=jpeg',
+        'https://mmbiz.qpic.cn/mmbiz_jpg/M6nr7uuNic7tU2yAMibJrW12C1bFPOo6LIdTibqKIiba4iagQb12ezjGYr8elKcFxGrp3yiaqC1tfTMJ9X45ooYxvjFQ/0?wx_fmt=jpeg'
       ]
     });
   };
